376347de7de87d1e880a1fbb6157e61d103885e9
stantheman21ph <54230298+stantheman21ph@users.noreply.github.com>
2021-10-26 01:07:23 +0800

    Update Code to Latest Changes

--- ProjectMo.SQLDataAccess/AccountProcess.cs
+++ ProjectMo.SQLDataAccess/AccountProcess.cs
@@ -21,8 +21,8 @@ namespace ProjectMo.SQLDataAccess
                 //myAccount = context.Accounts.FirstOrDefault(x => x.UserName == UserName)
                 var myAccountId = context.Accounts.FirstOrDefault(x => x.UserName == UserName).AccountId;
                 var myPaymentList = (from p in context.Payments
-                                     join b in context.Balances on p.AccountId equals b.AccountId
-                                     where p.PaymentId == 1
+                                     join b in context.Balances on p.PaymentId equals b.PaymentId
+                                     where p.AccountId == myAccountId
                                      select new PaymentList
                                      {
                                         PaymentDate = p.PaymentDate.ToString(),
@@ -39,7 +39,28 @@ namespace ProjectMo.SQLDataAccess
         public AccountDetails GetAccountBalanceFromSQL(string UserName, string PaymentDate)
         {
             AccountDetails myAccount = new AccountDetails();
+            List<PaymentList> myPayments = new List<PaymentList>();
 
+            DateTime PaymentDateConverted = DateTime.Parse(PaymentDate);
+            using (var context = new ProjectMDatabaseEntities())
+            {
+                myAccount.UserName = UserName;
+                //myAccount = context.Accounts.FirstOrDefault(x => x.UserName == UserName)
+                var myAccountId = context.Accounts.FirstOrDefault(x => x.UserName == UserName).AccountId;
+                var myPaymentList = (from p in context.Payments
+                                     join b in context.Balances on p.PaymentId equals b.PaymentId
+                                     where p.AccountId == myAccountId && p.PaymentDate >= PaymentDateConverted
+                                     select new PaymentList
+                                     {
+                                         PaymentDate = p.PaymentDate.ToString(),
+                                         Amount = p.Amount ?? 0,
+                                         Balance = b.BalanceAmount ?? 0,
+                                         DueDate = b.BalanceDueDate.ToString()
+                                     }).Distinct().ToList().OrderBy(x => x.PaymentDate);
+                myPayments.AddRange(myPaymentList);
+            }
+
+            myAccount.Payments = myPayments;
             return myAccount;
         }
     }
