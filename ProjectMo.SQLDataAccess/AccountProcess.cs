97af15a2020edf581f1e75dfb95ab2815fba6acd
stantheman21ph <54230298+stantheman21ph@users.noreply.github.com>
2021-10-26 01:11:17 +0800

    Code Update

--- ProjectMo.SQLDataAccess/AccountProcess.cs
+++ ProjectMo.SQLDataAccess/AccountProcess.cs
@@ -14,54 +14,70 @@ namespace ProjectMo.SQLDataAccess
         {
             AccountDetails myAccount = new AccountDetails();
             List<PaymentList> myPayments = new List<PaymentList>();
-
-            using (var context = new ProjectMDatabaseEntities())
+            try 
             {
-                myAccount.UserName = UserName;
-                //myAccount = context.Accounts.FirstOrDefault(x => x.UserName == UserName)
-                var myAccountId = context.Accounts.FirstOrDefault(x => x.UserName == UserName).AccountId;
-                var myPaymentList = (from p in context.Payments
-                                     join b in context.Balances on p.PaymentId equals b.PaymentId
-                                     where p.AccountId == myAccountId
-                                     select new PaymentList
-                                     {
-                                        PaymentDate = p.PaymentDate.ToString(),
-                                        Amount = p.Amount ?? 0,
-                                        Balance = b.BalanceAmount ?? 0,
-                                        DueDate = b.BalanceDueDate.ToString()
-                                     }).Distinct().ToList().OrderBy(x => x.PaymentDate);
-                myPayments.AddRange(myPaymentList);
-             }
+                using (var context = new ProjectMDatabaseEntities())
+                {
+                    myAccount.UserName = UserName;
+                    //myAccount = context.Accounts.FirstOrDefault(x => x.UserName == UserName)
+                    var myAccountId = context.Accounts.FirstOrDefault(x => x.UserName == UserName).AccountId;
+                    var myPaymentList = (from p in context.Payments
+                                         join b in context.Balances on p.PaymentId equals b.PaymentId
+                                         where p.AccountId == myAccountId
+                                         select new PaymentList
+                                         {
+                                             PaymentDate = p.PaymentDate.ToString(),
+                                             Amount = p.Amount ?? 0,
+                                             Balance = b.BalanceAmount ?? 0,
+                                             DueDate = b.BalanceDueDate.ToString()
+                                         }).Distinct().ToList().OrderBy(x => x.PaymentDate);
+                    myPayments.AddRange(myPaymentList);
+                }
 
-            myAccount.Payments = myPayments;
-            return myAccount;
+                myAccount.Payments = myPayments;
+                return myAccount;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                return myAccount;
+            }
+            
         }
         public AccountDetails GetAccountBalanceFromSQL(string UserName, string PaymentDate)
         {
             AccountDetails myAccount = new AccountDetails();
             List<PaymentList> myPayments = new List<PaymentList>();
 
-            DateTime PaymentDateConverted = DateTime.Parse(PaymentDate);
-            using (var context = new ProjectMDatabaseEntities())
+            try
             {
-                myAccount.UserName = UserName;
-                //myAccount = context.Accounts.FirstOrDefault(x => x.UserName == UserName)
-                var myAccountId = context.Accounts.FirstOrDefault(x => x.UserName == UserName).AccountId;
-                var myPaymentList = (from p in context.Payments
-                                     join b in context.Balances on p.PaymentId equals b.PaymentId
-                                     where p.AccountId == myAccountId && p.PaymentDate >= PaymentDateConverted
-                                     select new PaymentList
-                                     {
-                                         PaymentDate = p.PaymentDate.ToString(),
-                                         Amount = p.Amount ?? 0,
-                                         Balance = b.BalanceAmount ?? 0,
-                                         DueDate = b.BalanceDueDate.ToString()
-                                     }).Distinct().ToList().OrderBy(x => x.PaymentDate);
-                myPayments.AddRange(myPaymentList);
-            }
+                DateTime PaymentDateConverted = DateTime.Parse(PaymentDate);
+                using (var context = new ProjectMDatabaseEntities())
+                {
+                    myAccount.UserName = UserName;
+                    //myAccount = context.Accounts.FirstOrDefault(x => x.UserName == UserName)
+                    var myAccountId = context.Accounts.FirstOrDefault(x => x.UserName == UserName).AccountId;
+                    var myPaymentList = (from p in context.Payments
+                                         join b in context.Balances on p.PaymentId equals b.PaymentId
+                                         where p.AccountId == myAccountId && p.PaymentDate >= PaymentDateConverted
+                                         select new PaymentList
+                                         {
+                                             PaymentDate = p.PaymentDate.ToString(),
+                                             Amount = p.Amount ?? 0,
+                                             Balance = b.BalanceAmount ?? 0,
+                                             DueDate = b.BalanceDueDate.ToString()
+                                         }).Distinct().ToList().OrderBy(x => x.PaymentDate);
+                    myPayments.AddRange(myPaymentList);
+                }
 
-            myAccount.Payments = myPayments;
-            return myAccount;
+                myAccount.Payments = myPayments;
+                return myAccount;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return myAccount;
+            }           
         }
     }
 }
